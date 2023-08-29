296e687d055bbfab30494d3ecb87ed86e427eb18
DiegoVsp <diego-vespa2020@outlook.com.br>
2023-08-29 11:02:03 -0300

    atualizando aulas JS

---
+++ Aula JS/aula10/index.js
@@ -0,0 +1,33 @@
+// Operadores aritméticos
+// + -> Adição / Concatenação
+// ** -> Potenciação 
+// % Resto da divisão
+
+// const num1 = 2;
+// const num2 = 10;
+
+// console.log(num1 + num2);
+// console.log(num1 / num2);
+// console.log(num1 * num2);
+// console.log(num1 - num2);
+// console.log(num1 ** num2); // POTENCIAÇÃO
+// console.log(num1 % num2); // Resto da divisão
+
+// let contador = 10;
+// let passo = 5;
+// // contador++//operador de incremento
+// // contador--//operador de decremento
+
+// contador = contador + passo
+// console.log(contador)
+
+// contador += passo // contador = contador + passo
+// console.log(contador)
+
+let num1 = 5
+// let num2 = parseInt('10') // convertendo em um número inteiro
+let num2 = Number('10') // convertendo em um número inteiro
+console.log(num1 + num2)
+
+
+
---
+++ Aula JS/aula11/index.js
@@ -0,0 +1,27 @@
+// alert(`Olá`)
+// let confirma = confirm('Deseja realmente confirmar?') //Confirm retorna um boolean(true or false)
+// alert(confirma)
+
+//Exercício
+// let num1 = prompt('Digite um número: ')
+// let num2 = prompt('Digite um segundo número: ')
+
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+
+
+// alert(`O resultado foi: ${num1 + num2}`)
+
+// alert('Olá mundo!')
+
+let varA = 'A'
+let varB = 'B'
+let varC = 'C'
+let v;
+console.log(varA,varB,varC)
+v = varA
+varA = varB
+varB = varC
+varC = v
+
+console.log(varA,varB,varC)
