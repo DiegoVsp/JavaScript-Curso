2a3b983859467cf968635b8179653515b6f33709
DiegoVsp <diego-vespa2020@outlook.com.br>
2023-08-31 01:32:49 -0300

    atualizando aulas/exercicio

--- Aula JS/aula12/exercicio/index.js
+++ Aula JS/aula12/exercicio/index.js
@@ -1,9 +1,26 @@
+//************AULA 13 EXERCÍCIO */
+// //Diego
+// alert(`Seu nome tem ${nome.length} letras`)
+// alert(`A segunda letra do seu nome é ${nome.slice(1,2)}`)
+// alert(`Qual o primeiro índice da letra LETRA no seu nome? `)
+// alert(`Qual o último índice da letra LETRA no seu nome? `)
+// alert(`As últimas 3 letras do seu nome são: ${nome.slice(-3)}`)
+// alert(`Seu nome com letras maiúsculas: ${nome.toUpperCase()}`)
+// alert(`Seu nome com letras minúsculas: ${nome.toLowerCase()}`)
 let nome = prompt('Digite seu nome: ')
-//Diego
-alert(`Seu nome tem ${nome.length} letras`)
-alert(`A segunda letra do seu nome é ${nome.slice(1,2)}`)
-alert(`Qual o primeiro índice da letra LETRA no seu nome? `)
-alert(`Qual o último índice da letra LETRA no seu nome? `)
-alert(`As últimas 3 letras do seu nome são: ${nome.slice(-3)}`)
-alert(`Seu nome com letras maiúsculas: ${nome.toUpperCase()}`)
-alert(`Seu nome com letras minúsculas: ${nome.toLowerCase()}`)
+
+document.body.innerHTML += `O seu nome é: <strong>${nome}</strong><br/>`
+document.body.innerHTML += `Seu nome tem: <strong>${nome.length}</strong> letras <br/>`
+document.body.innerHTML += ` A segunda letra do seu nome é <strong>${nome.charAt(1)}</strong> <br/>`
+document.body.innerHTML += `Qual o primeiro índice da letra D no seu nome? <strong>${nome.indexOf('D')}</strong> <br/>`
+document.body.innerHTML += `Qual o último índice da letra A no seu nome? <strong>${nome.lastIndexOf('a')}</strong> <br/>`
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong> <br/>`
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong><br/>`
+// document.body.innerHTML += `${nome.replace(/ /g, ' + ')}<br/>`
+
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br/>`
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br/>`
+
+
+
+
--- Aula JS/aula12/index.js
+++ Aula JS/aula12/index.js
@@ -3,7 +3,7 @@
 //               01234567
 let umaString = 'Um texto';
 let fraseRato = 'O rato roeu a roupa do rei de roma.'
-// console.log(umaString.charAt(6));
+console.log(umaString.charAt(6));
 // console.log(umaString.concat(` em um lindo dia.`));
 // console.log(`${umaString} em um lindo dia.`);
 
