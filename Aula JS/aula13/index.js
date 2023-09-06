96cef0fffe25c35b3ed3bf084e941764df2ce052
DiegoVsp <diego-vespa2020@outlook.com.br>
2023-09-05 23:31:24 -0300

    atualizando aulas

---
+++ Aula JS/aula13/index.js
@@ -0,0 +1,21 @@
+let num1 = 10.2
+let num2 = 6
+
+// num1 = num1.toString() // convertendo em tipo string
+// console.log(num1.toString() + num2) // transformando temporariamente em String
+// console.log(typeof num1) // o tipo de num1 ainda continua sendo do tipo number
+// console.log(`Tipo binário de ${num1} = ${num1.toString(2)}`)
+
+// console.log(typeof num1)
+
+// console.log(num1.toFixed(2)) // arredonda o número para 2 casas decimais
+
+// console.log(Number.isInteger(num1)) // verificar se o número que a variável num1 recebe é um número inteiro
+
+// let temp = num1*'ola';
+// console.log(temp)
+// console.log(Number.isNaN(temp))
+
+num1 += num2
+console.log(parseInt(num1))
+console.log(parseFloat(num1))
